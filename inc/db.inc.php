3d74f1efdb418574d6a50d8bf75bb47caee0c4ca
Brenda <einstenfania@gmail.com>
2018-08-22 12:41:14 +1000

    Revert "Initial commit"
    This reverts commit 50e08f05b054bb77d701f3c9d354aebf05f649e1.

--- inc/db.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$con = mysqli_connect("localhost","iaemembe_kevin","1qazxsw2!@"); // server
-//$con = mysqli_connect("localhost","root","");
-$con= mysqli_connect("198.1.65.140","iaemembe_brenda","iaebris1899");// local
-mysqli_set_charset($con, 'utf8');
-mysqli_select_db($con, "iaemembe_soldb_co")or die( "<p><span style=\"color: red;\">Unable to select database</span></p>");
-?>
